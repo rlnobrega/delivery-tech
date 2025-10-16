71a3f6dae0ea55e5559b64e225aedf1ca5dd7c21
Rodrigo Luiz Pereira da Silva Nobrega <rluiznobrega@gmail.com>
2025-10-16 20:07:55 -0300

    Primeiro commit do projeto Delivery API

---
+++ src/test/java/com/delivery_tech/delivery_api/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery_tech.delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
