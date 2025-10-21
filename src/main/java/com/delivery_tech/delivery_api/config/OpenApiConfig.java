76c0cd000724be869bf72ff955f502ca7ebf33ea
Rodrigo Luiz Pereira da Silva Nobrega <rluiznobrega@gmail.com>
2025-10-20 22:17:07 -0300

    Inclusão do Swagger OpenAPI e configuração de segurança JWT

---
+++ src/main/java/com/delivery_tech/delivery_api/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.delivery_tech.delivery_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Delivery API")
+                        .version("1.0.0")
+                        .description("Delivery API de autenticação com JWT"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+
+}
