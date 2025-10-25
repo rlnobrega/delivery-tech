1bfe74a2578e176b4d29a4a3bd84c21c2ae016f4
Rodrigo Luiz Pereira da Silva Nobrega <rluiznobrega@gmail.com>
2025-10-24 22:20:13 -0300

    Incluido o arquivo RegisterRequest.java para gerenciar os dados de registro de novos usuários na API de entrega e o dto contém campos essenciais como nome, email, senha e endereço.

---
+++ src/main/java/com/delivery_tech/delivery_api/dto/request/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.delivery_tech.delivery_api.dto.request;
+
+import com.delivery_tech.delivery_api.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+    private String senha;
+
+    @NotBlank(message = "Telefone é obrigatório")
+    private Role role;
+
+}
